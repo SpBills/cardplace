74c524af93b8bab3c13c2981729a184fb6a469a5
SpBills <spencebills0@gmail.com>
2022-12-22 14:32:34 -0500

    Rating stuff

--- components/carddisplay.tsx
+++ components/carddisplay.tsx
@@ -1,8 +1,9 @@
 import { Card } from "@prisma/client"
+import Link from "next/link"
 import { PropsWithChildren } from "react"
 export default ({ card: { uid, image, name, price } }: { card: Card }) => {
   return (
-    <a href={`/cards/${uid}`} className="group">
+    <Link href={`/cards/${uid}`} className="group">
       <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg sm:aspect-w-2 sm:aspect-h-3">
         <img src={image} alt="Person using a pen to cross a task off a productivity paper card." className="h-full w-full object-cover object-center group-hover:opacity-75" />
       </div>
@@ -10,6 +11,6 @@ export default ({ card: { uid, image, name, price } }: { card: Card }) => {
         <h3>{name}</h3>
         <p>${price}</p>
       </div>
-    </a>
+    </Link>
   )
 }
